e31b5c33d88a40527f860d50bcd9da63587b2d9b
tavin <otaviohero@gmail.com>
2024-07-04 22:15:34 -0300

    adicionando o header

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]')
-   
 
+    const hero = document.querySelector('.hero');
+    const alturaHero = hero.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementos();
+        } else {
+            exibeElementos();
+        }
+    })
+
+
+    //seçao de atraçoes, programaçao das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
         const abaAlvo = botao.target.dataset.tabButton;
@@ -14,10 +28,21 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 
+    //seçao faq, accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFecha)
     }
 })
+
+function ocultaElementos() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementos() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
 
 function abreOuFecha(elemento) {
     const classe = 'faq__questions__item--is-open';
